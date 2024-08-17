8ce205c00909910fad3518d0ffe3b9890eb46e71
Bader Idris <baidsforbusiness@gmail.com>
2024-08-17 15:41:00 +0300

    fixed: error code view got created for maintenance devSecOps

--- project/src/router/index.ts
+++ project/src/router/index.ts
@@ -1,20 +1,27 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
 
+const routes = [
+  {
+    path: "/",
+    name: "home",
+    component: HomeView
+  },
+  {
+    path: "/country/:name",
+    name: "countryName",
+    component: () => import("../views/SpecificCountry.vue")
+  },
+  {
+    path: "/:pathMatch(.*)*",
+    name: "NotFound",
+    component: () => import("@/views/404.vue")
+  }
+];
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: "/",
-      name: "home",
-      component: HomeView
-    },
-    {
-      path: "/country/:name",
-      name: "countryName",
-      component: () => import("../views/SpecificCountry.vue")
-    }
-  ]
+  routes
 });
 
 export default router;
---
+++ project/src/views/404.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="not-found">
+    <p class="text">404</p>
+    <router-link to="/">
+      <span> back to main page </span>
+    </router-link>
+  </div>
+</template>
+
+<style lang="scss">
+.not-found {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  // margin-top: -50px;
+  font-weight: bold;
+  background-color: $very-dark-blue-text;
+  height: calc(100vh - 80px);
+  .text {
+    font-size: 150px;
+    margin-top: 0;
+    margin-bottom: 5px;
+    color: $White;
+  }
+  a {
+    font-size: 32px;
+    text-transform: capitalize;
+    color: $dark-gray;
+  }
+}
+</style>
